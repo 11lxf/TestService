b31e7a02256de684fdf7a28292a69bfdba5b59b7
shawf <li823311847@gmail.com>
2025-05-22 00:19:03 +0800

    修改mvn setting 文件路径，多写了一个.

--- .github/workflows/build/buildAndPublish.sh
+++ .github/workflows/build/buildAndPublish.sh
@@ -15,16 +15,20 @@ if [ "${isRelease}"x = "false"x ]; then
   # 压缩包名称
   # PACKAGE_TAR_PATH="${SERVICE_NAME}_${SERVICE_VERSION}.${buildNumber}"
 
-  # 执行工程编译
+  # 用于获取当前脚本所在的绝对路径
   workdir=$(
-    # $0 输出的是脚本名称，$(dirname $0) 输出的是当前目录到脚本的相对路径（bash 比如 /opt/test.sh 输出的就是 /opt/test.sh）
+    # $0 输出bash后面的第一个参数，即脚本名称，如 bash /opt/test/test.sh即输出 /opt/test/test.sh; bash test.sh 即输出 test.sh
+    # dirname命令用于去除文件路径名中的最后一个斜杠（/）及其之后的所有字符，从而获取文件所在的目录路径
+    # $(dirname $0) 输出的是当前目录到脚本的相对路径（比如 bash /opt/test/test.sh 输出的就是 /opt/test; bash test.sh 输出的就是.）
+    # 所以cd $(dirname $0) 切换到脚本所在目录
     cd $(dirname $0)
+    # 然后输出当前目录的绝对路径
     pwd
   )
-  # 此时相对路径在 .github/workflows/
+  # 此时相对路径在 .github/workflows/build 的上一层目录 .github/workflows
   cd $workdir/..
   # maven打包命令
-  mvn clean deploy --settings .https_settings.xml -Dmaven.test.skip=true -DfinalName=${PACKAGE_NAME}
+  mvn clean deploy --settings https_settings.xml -Dmaven.test.skip=true -DfinalName=${PACKAGE_NAME}
 elif [ "${isRelease}"x = "true"x ]; then
   SERVICE_VERSION=${releaseVersion}
   # 版本号+时间戳+build随机数写入buildInfo.properties

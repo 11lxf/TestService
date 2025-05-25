6ea990b0d70c5d3596de5547ec3db7f782609b31
shawf <li823311847@gmail.com>
2025-05-25 19:18:57 +0800

    test-pipeline

--- .github/workflows/deploy/0-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "0.停止相关进程..."
-PID=$(ps -ef | grep $ARTIFACT_ID-$VERSION.jar | grep -v grep | awk '{ print $2 }')
-if [ -n "$PID" ]; then
-    echo "Killing process with PID $PID"
-    kill -9 $PID
-fi
--- .github/workflows/deploy/1-uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "0.删除对应文件..."
-find /opt/apps/TestService/ \( -name '*.jar' -o -name 'maven-metadata.xml' \) -delete

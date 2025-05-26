8043b59a47d8a706f7f74aea7310df18257381ca
shawf <li823311847@gmail.com>
2025-05-26 23:34:19 +0800

    test-pipeline

--- .github/workflows/build/buildAndPublish.sh
+++ .github/workflows/build/buildAndPublish.sh
@@ -7,13 +7,15 @@ set -ex
 echo "Release is ${IS_RELEASE}"
 # 判断当前构建是否为版本构建，以及定义构建变量(包版本,包服务名称,包编译存放路径,包类型,包编译名称,包打包名称)
 if [ "${IS_RELEASE}"x = "false"x ]; then
-  SERVICE_VERSION='1.0.0-SNAPSHOT'
-  # 保存版本变脸到输出变量version
-  echo "version=${SERVICE_VERSION}" >> $GITHUB_OUTPUT
+  # 保存版本变量到输出变量version
+  echo "version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)" >> $GITHUB_OUTPUT
   # maven打包并发布到私仓
   mvn clean deploy --settings .github/workflows/https_settings.xml
 elif [ "${IS_RELEASE}"x = "true"x ]; then
-  SERVICE_VERSION=${releaseVersion}
+  # 保存版本变量到输出变量version
+  echo "version=${RELEASE_VERSION}" >> $GITHUB_OUTPUT
+  # 设定 release 版本, -DgenerateBackupPoms=false 表示不生成备份文件
+  mvn versions:set -DnewVersion=${RELEASE_VERSION} -DgenerateBackupPoms=false
   # maven打包并发布到私仓, 主仓合入代码前必须保证所有测试通过，所以发布正式环境时跳过单元测试
-  mvn clean deploy --settings .github/workflows/https_settings.xml -Dmaven.test.skip=true -U -Dmaven.wagon.http.ssl.insecure=true
+  mvn clean deploy --settings .github/workflows/https_settings.xml -Dmaven.test.skip=true
 fi
